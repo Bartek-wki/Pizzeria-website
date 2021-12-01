51ddbe47a2577d341e7d75f1c2c7bc01de0a481a
Bartosz Wilki <b.wilki0109@gmail.com>
2021-12-01 22:13:59 +0100

    8.6

--- src/js/script.js
+++ src/js/script.js
@@ -60,7 +60,13 @@
 
       thisProduct.renderInMenu();
 
+      thisProduct.getElement();
+
       thisProduct.initAccordion();
+
+      thisProduct.InitOrderForm();
+
+      thisProduct.processOrder();
 
       console.log('new Product:', thisProduct);
     }
@@ -83,15 +89,25 @@
 
     }
 
+    getElement() {
+      const thisProduct = this;
+
+      thisProduct.accordionTrigger = thisProduct.element.querySelector(select.menuProduct.clickable);
+      thisProduct.form = thisProduct.element.querySelector(select.menuProduct.form);
+      //console.log(thisProduct.form);
+      thisProduct.formInputs = thisProduct.form.querySelectorAll(select.all.formInputs);
+      console.log(thisProduct.formInputs);
+      thisProduct.cartButton = thisProduct.element.querySelector(select.menuProduct.cartButton);
+      //console.log(thisProduct.cartButton);
+      thisProduct.priceElem = thisProduct.element.querySelector(select.menuProduct.priceElem);
+      //console.log(thisProduct.priceElem);
+    }
+
     initAccordion() {
       const thisProduct = this;
-     
-      /* find the clickable trigger (the element that should react to clicking) */
-      const clickableTrigger = thisProduct.element.querySelector(select.menuProduct.clickable);
-      console.log(clickableTrigger);
-
+      
       /* START: add event listener to clickable trigger on event click */
-      clickableTrigger.addEventListener('click', function (event) {
+      thisProduct.accordionTrigger.addEventListener('click', function (event) {
         //console.log('Link was clicked');
       
         /* prevent default action for event */
@@ -112,7 +128,75 @@
         thisProduct.element.classList.toggle(classNames.menuProduct.wrapperActive);
       });
     }
+
+    InitOrderForm() {
+      const thisProduct = this;
+      //console.log(thisProduct);
+
+      thisProduct.form.addEventListener('submit', function (event) {
+        event.preventDefault();
+        thisProduct.processOrder();
+      });
+
+      for (let input of thisProduct.formInputs) {
+        input.addEventListener('change', function () {
+          thisProduct.processOrder();
+        });
+      }
+
+      thisProduct.cartButton.addEventListener('click', function (event) {
+        event.preventDefault();
+        thisProduct.processOrder();
+      });
+    }
     
+    processOrder() {
+      const thisProduct = this;
+      //console.log(thisProduct);
+
+      const formData = utils.serializeFormToObject(thisProduct.form);
+      console.log('formData', formData);
+
+      // set price to default price
+      let price = thisProduct.data.price;
+
+      // for every category (param)...
+      for (let paramId in thisProduct.data.params) {
+        // determine param value, e.q. paramId= 'toppings', param = {label: 'Toppings...}
+        const param = thisProduct.data.params[paramId];
+        console.log(paramId, param);
+
+        // for every option in this category
+        for (let optionId in param.options) {
+          const option = param.options[optionId];
+          console.log(optionId, option);
+
+          // check if optionId is selected
+          if (formData[paramId] && formData[paramId].includes(optionId)) {
+            console.log('select');
+
+            // check if option is default, if it is do nothing, if it isn't increase price
+            if (option.default == true) {
+              console.log('true');
+            } else {
+              console.log('false');
+              price += option.price;
+            }
+          } else {
+            console.log('unselect');
+
+            // check if option is default, if it is reduce price, if it isn't do nothing
+            if (option.default == true) {
+              price -= option.price;
+            }
+          }
+        }
+      }
+
+      // update calculated price in the HTML
+      thisProduct.priceElem.innerHTML = price;
+    
+    }
   }
 
   const app = {
