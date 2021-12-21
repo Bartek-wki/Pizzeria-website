9c88cd9a2e5c62cf5045e05c687084b1e8e8e814
Bartosz Wilki <b.wilki0109@gmail.com>
2021-12-21 19:31:26 +0100

    Edyt 10.4

--- src/js/components/Booking.js
+++ src/js/components/Booking.js
@@ -4,33 +4,26 @@ import AmountWidget from './AmountWidget.js';
 class Booking {
   constructor(element) {
     const thisBooking = this;
-
     thisBooking.render(element);
-    
     thisBooking.initWidgets();
   }
 
   render(element) {
     const thisBooking = this;
-
-    thisBooking.dom = {};
-
     const generatedHTML = templates.bookingWidget();
 
-    thisBooking.dom.wrapper = element;
+    thisBooking.dom = {
+      wrapper: element,
+      peopleAmount: document.querySelector(select.booking.peopleAmount),
+      hoursAmount: document.querySelector(select.booking.hoursAmount),
+    };
 
     thisBooking.dom.wrapper.innerHTML = generatedHTML;
-    
-    thisBooking.dom.peopleAmount = document.querySelector(select.booking.peopleAmount);
-
-    thisBooking.dom.hoursAmount = document.querySelector(select.booking.hoursAmount);
   }
 
   initWidgets() {
     const thisBooking = this;
-
     thisBooking.peopleAmountWidget = new AmountWidget(thisBooking.dom.peopleAmount);
-
     thisBooking.hoursAmountWidget = new AmountWidget(thisBooking.dom.hoursAmount);
 
     thisBooking.dom.peopleAmount.addEventListener('clink', function () {
